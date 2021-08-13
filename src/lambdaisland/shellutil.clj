dcb5bf1464f45f23ff276f454182c8a9c744758d
Arne Brasseur <arne@arnebrasseur.net>
2021-08-13 13:37:46 +0200

    First commit
    Extracted from lambdaisland/open-source

---
+++ src/lambdaisland/shellutil.clj
@@ -0,0 +1,137 @@
+(ns lambdaisland.shellutil
+  "Globbing and other shell-like filename handling
+
+  Extracted from https://github.com/lambdaisland/open-source and further improved"
+  (:require [clojure.java.io :as io])
+  (:import (java.io File)
+           (java.nio.file Paths)))
+
+(def ^:dynamic *cwd*
+  "Current working directory
+
+  Relative paths are resolved starting from this location. Defaults to the CWD
+  of the JVM, as exposed through the 'user.dir' property."
+  (System/getProperty "user.dir"))
+
+(defmacro with-cwd
+  "Execute `body` with [[*cwd*]] set to `cwd`."
+  [cwd & body]
+  `(let [prev# *cwd*
+         cwd# (File. *cwd* ~cwd)]
+     (binding [*cwd* cwd#]
+       (try
+         (System/setProperty "user.dir" (str cwd#))
+         ~@body
+         (finally
+           (System/setProperty "user.dir" prev#))))))
+
+(defn absolute?
+  "The File contains an absolute path"
+  [^File f]
+  (.isAbsolute f))
+
+(defn relative?
+  "The File contains a relative path"
+  [^File f]
+  (not (.isAbsolute f)))
+
+(defn file
+  "java.io.File constructor
+
+  Resolves relative paths relative to [[*cwd*]]"
+  [path]
+  (let [f (io/file path)]
+    (if (.isAbsolute f)
+      f
+      (io/file *cwd* path))))
+
+(defn- glob->regex
+  "Takes a glob-format string and returns a regex."
+  [s]
+  (loop [stream s
+         re ""
+         curly-depth 0]
+    (let [[c j] stream]
+      (cond
+        (nil? c) (re-pattern (str "^" (if (= \. (first s)) "" "(?=[^\\.])") re "$"))
+        (= c \\) (recur (nnext stream) (str re c c) curly-depth)
+        (= c \/) (recur (next stream) (str re (if (= \. j) c "/(?=[^\\.])"))
+                        curly-depth)
+        (= c \*) (recur (next stream) (str re "[^/]*") curly-depth)
+        (= c \?) (recur (next stream) (str re "[^/]") curly-depth)
+        (= c \{) (recur (next stream) (str re \() (inc curly-depth))
+        (= c \}) (recur (next stream) (str re \)) (dec curly-depth))
+        (and (= c \,) (< 0 curly-depth)) (recur (next stream) (str re \|)
+                                                curly-depth)
+        (#{\. \( \) \| \+ \^ \$ \@ \%} c) (recur (next stream) (str re \\ c)
+                                                 curly-depth)
+        :else (recur (next stream) (str re c) curly-depth)))))
+
+(defn- get-root-file
+  [root-name]
+  (file (str root-name "/")))
+
+(defn ls
+  "List directory contents"
+  [^File dir]
+  (.listFiles dir))
+
+(defn parent-file ^File [^File dir]
+  (.getParentFile dir))
+
+(defn filter-files
+  "Filter list of files for names matching pattern re"
+  [files re]
+  (filter #(re-find re (.getName ^File %)) files))
+
+(defn canonicalize
+  "Return a canonical path
+
+  Resolves symlinks and makes relative paths absolute"
+  ^File [path]
+  (io/file (.getCanonicalPath (io/file path))))
+
+(defn relativize
+  "Turn an absolute path and a base path into a relative path"
+  ^File [base path]
+  (io/file (str (.relativize (Paths/get (str base) (into-array String []))
+                             (Paths/get (str path) (into-array String []))))))
+
+(defn mkdir-p
+  "Make directory including parents"
+  [dir]
+  (.mkdirs (io/file dir)))
+
+(defn glob
+  "Returns a seq of java.io.File instances that match the given glob pattern.
+  Ignores dot files unless explicitly included.
+
+  Examples: (glob \"*.{jpg,gif}\") (glob \".*\") (glob \"/usr/*/se*\")
+
+  Based on
+  https://github.com/jkk/clj-glob/blob/b1df67efb003f0e372c914346209d41c6df78e20/src/org/satta/glob.clj
+  "
+  [pattern]
+  (let [[root & _ :as parts] (.split #"[\\/]" pattern)
+        abs? (or (empty? root) ;unix
+                 (= \: (second root))) ;windows
+        home? (= "~" (first parts))
+        start-dir (cond
+                    abs? (get-root-file root)
+                    home? (io/file (System/getProperty "user.home"))
+                    :else (canonicalize ""))
+        patterns (if (or abs? home?) (rest parts) parts)]
+    (reduce
+     (fn [files pattern]
+       (cond
+         (= "." pattern)
+         (mapcat ls files)
+         (= ".." pattern)
+         (map parent-file files)
+         (= "**" pattern)
+         (filter #(.isDirectory %)
+                 (mapcat file-seq files))
+         :else
+         (mapcat #(filter-files (ls %) (glob->regex pattern)) files)))
+     [start-dir]
+     patterns)))
